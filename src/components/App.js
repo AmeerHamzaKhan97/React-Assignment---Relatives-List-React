c18512324ab5d15a52f94b0d51f1430e2a641dd5
Aquib <aquibshaikh02397@gmail.com>
2020-11-09 14:57:46 +0530

    code added

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,21 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(){
+        this.list = ["Aquib","Irfan","Hamza","Sufiyan","Hafiz","Arsalaan"];
+    }
     render() {
+        const listItem = this.list.map((item,i)=>{
+            <li key={"relative"+i} >{item}</li>
+        })
 
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               <ol>
+                   {listItem}
+               </ol>
+
             </div>
         )
     }
