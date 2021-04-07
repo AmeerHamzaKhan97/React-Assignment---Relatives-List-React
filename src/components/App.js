b16032f86c3467dd33230c9550c1235be7a416e6
AmeerHamzaKhan97 <hamzakn97@gmail.com>
2021-04-07 12:29:14 +0530

    first-react

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,21 @@
-import React, {Component, useState} from "react";
+import React from 'react';
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
-        const list = ["Aquib","Irfan","Hamza","Sufiyan","Hafiz","Arsalaan"];
-        const listItem = list.map((item,i)=>
-            <li key={"relativeListItem"+(i+1)} >{item}</li>
+class App extends React.Component{
+    render(){
+        const list = ["john","cena","triple","jeff","hardy"];
+        const listItem= list.map((item,i)=>
+        <li key ={"relativeListItem"+(i+1)}>{item}</li>
         );
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
-               <ol key="relativeList">
-                   {listItem}
-               </ol>
+                <ol key="relativeList">
+                    {listItem}
 
+                </ol>
             </div>
         )
     }
 }
-
-
 export default App;
